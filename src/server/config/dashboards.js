91dc56692ed0d613a43f889b4d5be7fbd4e77d6b
NeverOddOrEven <2084348+NeverOddOrEven@users.noreply.github.com>
2017-08-31 11:06:17 -0500

    Read dashboard configuration from file.
    Configuration is not committed to SCM

--- src/server/config/dashboards.js
+++ src/server/config/dashboards.js
@@ -1,10 +1,8 @@
 'use strict';
 
-module.exports = [
-  //{ url: 'http://10.230.214.41/view/Intelligent%20Fleet%20Builds/', seconds: 10 /* seconds */ },
-  { url: 'http://10.230.214.41/view/Intelligent%20Fleet%20Builds%20-%20v2/', seconds: 10 /* seconds */ },
-  { title: 'Intelligent Fleet Deploys', url: 'http://10.230.214.41/view/Intelligent%20Fleet%20Deploys/', seconds: 10 /* seconds */ },
-  { url: 'http://10.230.214.41/view/Intelligent%20Fleet%20Deploys%20-%20v2/', seconds: 10 /* seconds */ },
-  { title: 'Intelligent Fleet Deploys v3', url: 'http://10.230.214.41/view/Intelligent%20Fleet%20Deploys%20-%20v3/', seconds: 10 /* seconds */ },
-  { title: 'GitHub Orgs', url: 'http://10.230.214.41/view/GitHub%20Orgs/', seconds: 10 /* seconds */ },
-];
+const path = require('path');
+const fs = require('fs');
+
+const configuredDashboardsFile = path.join(__dirname, '../../..', 'local/dashboards.json');
+
+module.exports = JSON.parse(fs.readFileSync(configuredDashboardsFile, 'utf8'));
